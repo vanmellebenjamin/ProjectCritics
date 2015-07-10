e095865ec543189eb8dd3a30c97d476c207fd97e
BVanMelle <vanmelle.benjamin@gmail.com>
2015-07-10 23:20:41 +0200

    Login modifs

--- App/app/src/main/java/mouselab/projectcriticsandroid/Login.java
+++ App/app/src/main/java/mouselab/projectcriticsandroid/Login.java
@@ -1,13 +1,11 @@
 package mouselab.projectcriticsandroid;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +14,7 @@ import android.widget.TextView;
 import mouselab.projectcriticsandroid.mouselab.projectcriticsandroid.models.Requests;
 
 
-public class Login extends ActionBarActivity implements View.OnClickListener {
+public class Login extends Activity implements View.OnClickListener {
     private Button Login, Subscribe;
     private EditText Email, Password;
     private TextView Error;
@@ -52,28 +50,6 @@ public class Login extends ActionBarActivity implements View.OnClickListener {
             Password.setText(password);
             tryLoggin(email, password);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
    @Override
--- App/app/src/main/java/mouselab/projectcriticsandroid/MainActivity.java
+++ App/app/src/main/java/mouselab/projectcriticsandroid/MainActivity.java
@@ -1,7 +1,62 @@
 package mouselab.projectcriticsandroid;
 
+import android.hardware.Camera;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
 /**
  * Created by benjamin on 10.07.15.
  */
-public class MainActivity {
+public class MainActivity extends ActionBarActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions: " +
+                        e.getMessage());
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
 }
